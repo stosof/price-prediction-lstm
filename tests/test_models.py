e62a8ccfadddd11a9be615798e45bf89fa080a70
Sto Sof <stoyan.sofronov@gmail.com>
2019-01-14 09:44:31 +0200

    Update tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,14 +17,16 @@ class ModelsTests(unittest.TestCase):
 
     def test_get_training_data(self):
         lstm = LSTM_NN()
-        X, Y = lstm.get_training_data()
+        X, Y, X_val, Y_val = lstm.get_training_data()
         self.assertIsInstance(X, np.ndarray, "There was an issue getting X training data.")
         self.assertIsInstance(Y, np.ndarray, "There was an issue getting Y training data.")
+        self.assertIsInstance(X_val, np.ndarray, "There was an issue getting X training data.")
+        self.assertIsInstance(Y_val, np.ndarray, "There was an issue getting Y training data.")
 
     def test_fit_model(self):
         lstm = LSTM_NN()
-        X, Y = lstm.get_training_data()
-        history = lstm.fit_model(X=X, Y=Y, n_batch=1000, nb_epoch=1)
+        X, Y, X_val, Y_val = lstm.get_training_data()
+        history = lstm.fit_model(X=X, Y=Y, X_val=X_val, Y_val=Y_val, n_batch=1000, nb_epoch=1)
         self.assertIsInstance(history, keras.callbacks.History, "There was an issue training the model.")
 
 
